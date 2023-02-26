f924100e070cb4de7adebcadf46fcf5f5fcb3f34
EC2 Default User <ec2-user@ip-172-31-15-199.ap-northeast-1.compute.internal>
2023-02-26 15:35:42 +0000

    devise/ bootstrap導入, posts/homes controller作製

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'homes/top'
+  get 'homes/about'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
