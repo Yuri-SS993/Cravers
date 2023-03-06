668a7b694120ee98c9f2e3e25f3587572b0799af
EC2 Default User <ec2-user@ip-172-31-15-199.ap-northeast-1.compute.internal>
2023-03-06 06:43:53 +0000

    log in alertの表示

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     end
   end
   
-  resources :posts, only: [:index, :show, :create, :edit, :update, :destroy] do
+  resources :posts do
     resources :likes, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
